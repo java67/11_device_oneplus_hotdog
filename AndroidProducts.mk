6d50e5db65add634a5a78db186b48cb33e7de451
java67 <dakaizhe@yeah.net>
2020-10-04 23:07:02 +0800

    hotdog

---
+++ AndroidProducts.mk
@@ -0,0 +1,13 @@
+#
+# Copyright (C) 2019 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/yaap_guacamole.mk
+
+COMMON_LUNCH_CHOICES := \
+	yaap_guacamole-user \
+	yaap_guacamole-userdebug \
+	yaap_guacamole-eng
